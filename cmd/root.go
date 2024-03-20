4c3e33bf2448a21ac07c51e8b17b819f5c1c0895
dong9205 <920506213@qq.com>
2024-03-21 02:21:27 +0800

    add nix

--- cmd/root.go
+++ cmd/root.go
@@ -1,9 +1,6 @@
 package cmd
 
 import (
-	"github.com/dong9205/wechat_hook/pkg/config"
-	"github.com/dong9205/wechat_hook/pkg/logger"
-	"github.com/dong9205/wechat_hook/pkg/wx"
 	"github.com/spf13/cobra"
 )
 
@@ -13,15 +10,6 @@ var rootCmd = &cobra.Command{
 	Long:  `通过微信的接口发送消息，公众号推送消息`,
 	Run: func(cmd *cobra.Command, args []string) {
 	},
-}
-
-func init() {
-	// config
-	config.ConfigInit(rootCmd.Flags().String("config", "./config.yaml", "配置文件路径"))
-	// 日志
-	logger.InitLogger()
-	// wechat bot
-	_ = wx.GetBotMgr()
 }
 
 func Execute() {
--- cmd/serve.go
+++ cmd/serve.go
@@ -1,7 +1,10 @@
 package cmd
 
 import (
+	"github.com/dong9205/wechat_hook/pkg/config"
+	"github.com/dong9205/wechat_hook/pkg/logger"
 	"github.com/dong9205/wechat_hook/pkg/serve"
+	"github.com/dong9205/wechat_hook/pkg/wx"
 	"github.com/spf13/cobra"
 )
 
@@ -10,6 +13,12 @@ var serverCmd = cobra.Command{
 	Short: "启动服务",
 	Long:  `启动服务`,
 	Run: func(cmd *cobra.Command, args []string) {
+		// config
+		config.ConfigInit(rootCmd.Flags().String("config", "./config.yaml", "配置文件路径"))
+		// 日志
+		logger.InitLogger()
+		// wechat bot
+		_ = wx.GetBotMgr()
 		serve.Serve()
 	},
 }
